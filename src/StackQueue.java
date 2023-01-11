937b0c5eaf560adf014c9eb5acc36930a18c79c2
lillyphan <lphan24@kentdenver.org>
2023-01-11 10:55:38 -0700

    Add instance data, constructor, and int size method

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -1,11 +1,21 @@
-public class StackQueue {
-    //empty constructor
-    //int size
+public class StackQueue<E> {
+    private Stack<E> in;
+    private Stack<E> out;
+    private int size = 0;
+
+    public StackQueue(){
+        in = new Stack<>();
+        in = new Stack<>();
+    }
+
+    public int size(){
+        return size;
+    }
 
     //boolean isEqual
     //E enqueue
     //E dequeue
     //E peek
-    
+
     //toString
 }
